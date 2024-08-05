ab855fd7c9a9d6bf82ccbb2741afb60def94dfc4
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-08-05 18:43:29 -0300

    Add: All Mathematical Functions

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function calculator(a,operator,b){
+
+    if(a === null || a === undefined){
+        alert("Put some entry number");
+        return;
+    }
+    if(b === null || b === undefined){
+        alert("Put some other number to calc");
+        return;
+    }
+    if(operator === null || operator === undefined || operator === ""){
+        alert("Can't realize operation");
+        return;
+    }
+
     switch (operator) {
-        case "sum":
+        case "+":
             return a+b;
-        case "sub":
+        case "-":
             return a-b;
-        case "divide":
+        case "/":
             return a/b;
-        case "multi":
+        case "*":
             return a*b;
         default:
             break;
@@ -15,10 +29,14 @@ function calculator(a,operator,b){
 
 let numberA = 0;
 let numberB = 0;
-let operatorSeector = "";
+let operatorSelector = "";
+let hasOperator = false;
 
-let inputString = document.getElementById("#inputString");
 
+let inputString = document.querySelector("#inputString");
+
+
+// Add the numbers on screen
 function addToString(inputKey){
     switch (inputKey) {
         case "one":
@@ -48,11 +66,129 @@ function addToString(inputKey){
         case "nine":
             inputString.innerText += "9";
             break;
+        case "zero":
+            inputString.innerText += "0";
+            break;
+        case "sum":
+            inputString.innerText += "+";
+            break;
+        case "sub":
+            inputString.innerText += "-";
+            break;
+        case "divide":
+            inputString.innerText += "/";
+            break;
+        case "multi":
+            inputString.innerText += "*";
+            break;
         default:
             break;
     }
 }
 
-let oneBtn = document.getElementById("#container");
+function pickCalcString(stringToResolve){
+    if(stringToResolve === ""){
+        alert("No numbers to calc");
+    }
+    let toResolve = stringToResolve.split("");
+    let tempStringA = "";
+    let tempStringB = "";
+    let tempOperator = "";
 
-console.log(oneBtn)
+    for(let i = 0; i<toResolve.length; i++){
+        let numbfy = Number(toResolve[i]);
+        if(isNaN(numbfy)){
+            tempOperator = String(toResolve[i]);
+            break;
+        }
+        tempStringA += String(toResolve[i]);
+    }
+    
+    let tempOperatorIndex = toResolve.indexOf(tempOperator);
+
+    for(let i = tempOperatorIndex+1; i<toResolve.length; i++){
+        tempStringB += String(toResolve[i]);
+    }
+
+    numberA = Number(tempStringA);
+    numberB = Number(tempStringB);
+    operatorSelector = tempOperator;
+
+    let result = calculator(numberA,operatorSelector,numberB);
+    return result;
+    
+}
+
+// All Keys on the keyboard
+document.querySelector("#zero").addEventListener("mousedown", () => {
+    addToString("zero");
+});
+document.querySelector("#one").addEventListener("mousedown", () => {
+    addToString("one");
+});
+document.querySelector("#two").addEventListener("mousedown", () => {
+    addToString("two");
+});
+document.querySelector("#three").addEventListener("mousedown", () => {
+    addToString("three");
+});
+document.querySelector("#four").addEventListener("mousedown", () => {
+    addToString("four");
+});
+document.querySelector("#five").addEventListener("mousedown", () => {
+    addToString("five");
+});
+document.querySelector("#six").addEventListener("mousedown", () => {
+    addToString("six");
+});
+document.querySelector("#seven").addEventListener("mousedown", () => {
+    addToString("seven");
+});
+document.querySelector("#eight").addEventListener("mousedown", () => {
+    addToString("eight");
+});
+document.querySelector("#nine").addEventListener("mousedown", () => {
+    addToString("nine");
+});
+document.querySelector("#sum").addEventListener("mousedown", () => {
+    addOperatorToCalculator("sum");
+})
+document.querySelector("#divide").addEventListener("mousedown", () => {
+   addOperatorToCalculator("divide");
+})
+document.querySelector("#multiply").addEventListener("mousedown", () => {
+    addOperatorToCalculator("multi");
+})
+document.querySelector("#subtract").addEventListener("mousedown", () => {
+    addOperatorToCalculator("sub");
+})
+document.querySelector("#equals").addEventListener("mousedown", () => {
+    addOperatorToCalculator("equals");
+})
+document.querySelector("#deleteAll").addEventListener("mousedown", () => {
+    inputString.innerText = "";
+    hasOperator = false;
+})
+document.querySelector("#deleteOne").addEventListener("mousedown", () => {
+    let allTheString = inputString.innerText
+    let splitted = allTheString.split("");
+    splitted.pop();
+    if(splitted == []){
+        inputString.innerText = "";
+    }
+    let removed = splitted.join("");
+    inputString.innerText = removed;
+    hasOperator = false;
+})
+
+function addOperatorToCalculator(operator){
+    if(!hasOperator && operator != "equals"){
+        addToString(operator);
+        hasOperator = true;
+    }else{
+        let calcResult = pickCalcString(inputString.innerText)
+        if(calcResult === undefined || calcResult === null) return;
+        inputString.innerText = calcResult;
+        hasOperator = false;
+    }
+}
